6ccbef5e92cec0e2423bf4fe5a24c31bc0b70aee
Tasha Casagni <casagni708@hotmail.com>
2017-11-11 17:08:09 -0500

    updated styles and built out additional components to html page for itinerary

--- src/ItineraryPage/containers/ItineraryPageContainer.js
+++ src/ItineraryPage/containers/ItineraryPageContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import '../styles/Itinerary.css';
 import { Col } from 'react-bootstrap';
 import { Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 // import logo from './logo.svg';
 // import './App.css';
 
@@ -16,6 +17,20 @@ class App extends Component {
           <Col sm={6} md={6}>
             <p> hi</p>
           </Col>
+        </Row>
+        <Row className="a-links">
+          <i class="fa fa-heart-o" aria-hidden="true" />
+          <i class="fa fa-heart" aria-hidden="true" />
+          <Link to="#">Rehersal Dinner XX/XX/XX XXPM</Link>
+          <i class="fa fa-heart" aria-hidden="true" />
+          <i class="fa fa-heart-o" aria-hidden="true" />
+          <Link to="#">Ceremony XX/XX/XX XXPM</Link>
+          <i class="fa fa-heart-o" aria-hidden="true" />
+          <i class="fa fa-heart" aria-hidden="true" />
+          <Link to="#">Reception XX/XX/XX XXPM</Link>
+          <i class="fa fa-heart" aria-hidden="true" />
+          <i class="fa fa-heart-o" aria-hidden="true" />
+          <Link to="#">Farewell Brunch XX/XX/XX XXPM</Link>
         </Row>
       </div>
     );
